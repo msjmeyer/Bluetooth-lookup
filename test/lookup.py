d90667ef5560d4a2ea8aaf78562867126943ea33
Marthinus <mmeyer@tradeninja.net>
2013-11-24 17:14:46 +0000

    Updated lookup.py

--- test/lookup.py
+++ test/lookup.py
@@ -10,29 +10,29 @@ while True:
 
     result = bluetooth.lookup_name('E8:3E:B6:F6:B4:32', timeout=5)
     if (result != None):
-        print "Ma Playbook: in"
+        print "Adam Playbook: in"
     else:
-        print "Ma Playbook: out"
+        print "Adam Playbook: out"
 
 
     result = bluetooth.lookup_name('AC:22:0B:60:39:E2', timeout=5)
     if (result != None):
-        print "Marthinus Nexus: in"
+        print "Bob Nexus: in"
     else:
-        print "Marthinus Nexus: out"
+        print "Bob Nexus: out"
 
     result = bluetooth.lookup_name('40:7A:80:B0:4F:35', timeout=5)
     if (result != None):
-        print "Marthinus Windows Phone: in"
+        print "Bob Windows Phone: in"
     else:
-        print "Marthinus Windows Phone: out"
+        print "Bob Windows Phone: out"
 
 
     result = bluetooth.lookup_name('C0:18:85:FC:7B:48', timeout=5)
     if (result != None):
-        print "Marthinus Laptop: in"
+        print "Bob Laptop: in"
     else:
-        print "Marthinus Laptop: out"
+        print "Bob Laptop: out"
 
 
     time.sleep(60)
